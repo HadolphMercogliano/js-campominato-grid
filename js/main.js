a97d82aac931d62db5f174e46e9d315869482652
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-01 20:00:43 +0100

    function working + html + css end

--- js/main.js
+++ js/main.js
@@ -9,5 +9,46 @@
 // - con difficoltà 2 => 81 caselle
 // - con difficoltà 3 => 49 caselle
 
+/******************************************************
+ *                        CODE ON LOAD                *
+ ******************************************************/
+const gridEl = document.getElementById("grid");
+console.log(gridEl);
+
+const playButtonEl = document.getElementById("play-button");
+
+const difficultyEasy = 100;
+const difficultyNormal = 81;
+const difficultyHard = 49;
+gridGenerator(gridEl, difficultyEasy);
 // TODO: creare un bottone
-//
+
+// TODO: creare una funzione che generi la griglia di gioco (parametri {griglia, dimensionemax})
+// generare all' interno della griglia una seguenza di numeri da 1 a dimensione
+
+// aggiungere una funzione al click sulla griglia che colora lo sfondo della casella di azzurro
+// stampa in console un messaggio con il numero contenuto nella casella
+
+/******************************************************
+ *                      FUNCTION                      *
+ ******************************************************/
+
+/**
+ * generatore di elementi della griglia
+ * @param {string} grid dove inserire le card
+ * @param {int} dimension quante card creare
+ */
+function gridGenerator(grid, dimension) {
+  for (let i = 0; i < dimension; i++) {
+    const cardEl = document.createElement("div");
+    cardEl.classList.add("grid-element");
+    cardEl.innerHTML = i + 1;
+    gridEl.append(cardEl);
+
+    console.log(cardEl);
+
+    cardEl.addEventListener("click", function () {
+      this.classList.toggle("active");
+    });
+  }
+}
